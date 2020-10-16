cdf73213712426c0a82c604c2060521e15e1761e
Marcelo Pinheiro <mpinheir@gmail.com>
2020-10-15 19:08:57 -0700

    Added comment to utfcheck.sh to explain functionality.

--- travis/utfcheck.sh
+++ travis/utfcheck.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Exit with an error code if the commmited .md files are not UTF-8 compliant.
 
 find . -name '*.md' -type f | while read fname; do
   if ! iconv -f UTF-8 "${fname}" -o /dev/null; then
